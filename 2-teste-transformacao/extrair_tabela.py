89e98b8881e7e4ec815d5ad3db927b0462aaf219
claudiohsc <claudio885henrique@gmail.com>
2025-03-26 22:08:12 -0300

    fix: bug de cabecalhos repetidos e colunas erradas

--- 2-teste-transformacao/extrair_tabela.py
+++ 2-teste-transformacao/extrair_tabela.py
@@ -5,17 +5,29 @@ anexo1 = "1-teste-web-scraping\Anexo_I.pdf"
 
 tabelas = tabula.read_pdf(anexo1, pages="3-181", multiple_tables=True)
 
-tabelas_limpa = []
+if not tabelas:
+    raise ValueError("Nenhuma tabela foi encontrada no PDF.")
 
-colunas_cabecalho = ["PROCEDIMENTO", "RN\n(alteração)","VIGÊNCIA","OD", "AMB", "HCO", "REF", "PAC", "DUT", "SUBGRUPO" "GRUPO", "CAPÍTULO"]
+# Pegar os nomes das colunas da primeira tabela como referência
+colunas_padrao = tabelas[0].columns.tolist()
 
-for tabela in tabelas:
-    if any(col in tabela.columns[0] for col in colunas_cabecalho):
-        tabela = tabela.iloc[1:]  # Remove a primeira linha se for cabeçalho
+tabelas_padronizadas = []
 
-    tabelas_limpa.append(tabela)
+for i, tabela in enumerate(tabelas):
+    
+    tabela = tabela.dropna(axis=1, how="all")
 
-# Concatenando todas as tabelas limpas
-df_final = pd.concat(tabelas_limpa, ignore_index=True)
+    # remover cabecalhos repetidos nas pags
+    if any(col in tabela.columns[0] for col in colunas_padrao):
+        tabela = tabela.iloc[1:] 
+    
+    # Garantir o cabecalho padrao
+    tabela = tabela.reindex(columns=colunas_padrao)
+
+    tabelas_padronizadas.append(tabela)
+
+df_final = pd.concat(tabelas_padronizadas, ignore_index=True)
 
 df_final.to_csv("2-teste-transformacao/tabela_unificada.csv", index=False)
+
+print("Arquivo salvo com sucesso!")
