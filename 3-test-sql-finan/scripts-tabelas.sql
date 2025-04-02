949d8cb37f3eea42a29dca683dfd98e99b9878af
claudiohsc <claudio885henrique@gmail.com>
2025-04-02 01:50:41 -0300

    add caminhos e arquvos + sql final

--- 3-test-sql-finan/scripts-tabelas.sql
+++ 3-test-sql-finan/scripts-tabelas.sql
@@ -32,3 +32,93 @@ CREATE TABLE financeiro (
     VL_SALDO_FINAL DECIMAL(15,2),
     FOREIGN KEY (REG_ANS) REFERENCES operadoras(Registro_ANS)
 );
+
+
+-- Importar dados das operadoras
+LOAD DATA INFILE '3-test-sql-finan\\Relatorio_cadop.csv'
+INTO TABLE operadoras
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(@Registro_ANS, @CNPJ, @Razao_Social, @Nome_Fantasia, @Modalidade, @Logradouro, @Numero, @Complemento, @Bairro, @Cidade, @UF, @CEP, @DDD, @Telefone, @Fax, @Endereco_eletronico, @Representante, @Cargo_Representante, @Regiao_de_Comercializacao, @Data_Registro_ANS)
+SET Data_Registro_ANS = STR_TO_DATE(@Data_Registro_ANS, '%Y-%m-%d');
+
+-- Import dos dados financeiros
+
+LOAD DATA INFILE '3-test-sql-finan\\financeiros\\1T2023.csv'
+INTO TABLE financeiro
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '3-test-sql-finan\\financeiros\\2T2023.csv'
+INTO TABLE financeiro
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '3-test-sql-finan\\financeiros\\3T2023.csv'
+INTO TABLE financeiro
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '3-test-sql-finan\\financeiros\\4T2023.csv'
+INTO TABLE financeiro
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '3-test-sql-finan\\financeiros\\1T2024.csv'
+INTO TABLE financeiro
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '3-test-sql-finan\\financeiros\\2T2024.csv'
+INTO TABLE financeiro
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '3-test-sql-finan\\financeiros\\3T2024.csv'
+INTO TABLE financeiro
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '3-test-sql-finan\\financeiros\\4T2024.csv'
+INTO TABLE financeiro
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+--  10 operadoras com maiores despesas em "EVENTOS/SINISTROS CONHECIDOS OU AVISADOS DE ASSISTÊNCIA À SAÚDE MÉDICO HOSPITALAR" no último trimestre
+SELECT f.REG_ANS, o.Razao_Social, SUM(f.VL_SALDO_FINAL) AS Total_Despesas
+FROM financeiro f
+JOIN operadoras o ON f.REG_ANS = o.Registro_ANS
+WHERE f.DESCRICAO LIKE '%EVENTOS/SINISTROS CONHECIDOS OU AVISADOS DE ASSISTÊNCIA À SAÚDE MÉDICO HOSPITALAR%'
+AND f.DATA >= DATE_SUB(CURDATE(), INTERVAL 3 MONTH)
+GROUP BY f.REG_ANS, o.Razao_Social
+ORDER BY Total_Despesas DESC
+LIMIT 10;
+
+-- 10 operadoras com maiores despesas nessa categoria no último ano
+SELECT f.REG_ANS, o.Razao_Social, SUM(f.VL_SALDO_FINAL) AS Total_Despesas
+FROM financeiro f
+JOIN operadoras o ON f.REG_ANS = o.Registro_ANS
+WHERE f.DESCRICAO LIKE '%EVENTOS/SINISTROS CONHECIDOS OU AVISADOS DE ASSISTÊNCIA À SAÚDE MÉDICO HOSPITALAR%'
+AND f.DATA >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
+GROUP BY f.REG_ANS, o.Razao_Social
+ORDER BY Total_Despesas DESC
+LIMIT 10;
